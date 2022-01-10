9373e5e55b54006aa8369d4a4b62e6b5fbd55fda
lmhyuck <49359991+lmhyuck@users.noreply.github.com>
2022-01-10 21:52:24 +0900

    [과제/02]02/lmhyuck (#10)
    * 문자열 압축 횟수 출력
    
    * 문자열 압축 횟수 출력
    
    * Delete Work-01.cpp
    
    * 1주차 과제
    
    * 2주차 과제
    
    * 2주차 과제 수정본
    
    * 2주차 과제 수정본
    
    * Delete 01.cpp
    
    * Delete README.md
    
    * Delete 01.cpp
    
    * 디렉터리 삭제
    
    * 2주차 과제
    
    * 2주차 과제 수정본

---
+++ 과제제출/lmhyuck/01/01.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(void) {
+    char arr[100];
+    int count = 1;
+    int i = 0;
+    cin.getline(arr, 100);
+    while (arr[i] != '\0') {
+
+        if (arr[i] == arr[i + 1])
+            count++;
+        else if (arr[i] != arr[i + 1]) {
+            if (count == 1)
+                cout << arr[i];
+            else {
+                cout << arr[i] << count;
+                count = 1;
+            }
+        }
+        i++;
+    }
+    cout << endl;
+    return 0;
+}
---
+++ 과제제출/lmhyuck/02/02.cpp
@@ -0,0 +1,106 @@
+#include <iostream>
+#include <string>
+#include <cstdlib>
+using namespace std;
+void sender(char* data, char* datasum);
+void Transmission_Process(char* datasum, char* redata);
+void Receiver(char* datasum, char* redata);
+int num = 0;
+
+int main()
+{
+    char* data, * datasum, * redata;
+    data = new char[5];
+    datasum = new char[6];
+    redata = new char[6];
+    sender(data, datasum);
+    Transmission_Process(datasum, redata);
+    Receiver(datasum, redata);
+    delete[] data;
+    delete[] datasum;
+    delete[] redata;
+    cout << endl;
+    return 0;
+}
+//입력받은 데이터를 checksum
+void sender(char* data, char* datasum) {
+    int sum=0;
+    cout << "Data : ";
+    cin >> data;
+    for (int i = 0; i < 5; i++) {
+        if (i == 4) {
+            datasum[4] = (sum % 10) + 48;
+            break;
+        }
+        datasum[i] = data[i];
+        sum += data[i] - 48;
+    }
+    cout << "Send Data: ";
+    for (int i = 0; i < 5; i++)
+        cout << datasum[i];
+}
+//데이터 전송 간 오류 발생 체크
+void Transmission_Process(char* datasum, char* redata) {
+    srand((unsigned int)time(NULL));
+    int n = 0;
+    n = rand() & 10;
+    if (n >= 0 && n < 4)
+    {
+        num++;
+        while (1)
+        {
+            int a = 0;
+            a = rand() & 4; //0~3까지
+            int b = 0;
+            b = rand() & 10;//0~9까지
+            for (int i = 0; i < 5; i++)
+            {
+                if (a == i)
+                {
+                    if (b == datasum[i])
+                    {
+                        continue;
+                    }
+                    else
+                        redata[i] = b + 48;
+                }
+                else
+                    redata[i] = datasum[i];
+            }
+            if(a==4)
+                num=2;
+            break;
+        }
+    }
+    else
+    {
+        for (int i = 0; i < 5; i++)
+            redata[i] = datasum[i];
+    }
+    cout << endl;
+}
+//전송받은 데이터의 에러 발생 유무 판단
+void Receiver(char* datasum, char* redata) {
+    cout << "Received Data: ";
+    if (num == 0) {
+        for (int i = 0; i < 5; i++)
+            cout << redata[i];
+        cout << endl << redata[4] << "==" << redata[4];
+        cout << "    Error is not Occured!";
+    }
+    else if (num==2) {
+        for (int i = 0; i < 5; i++)
+            cout << redata[i];
+        cout << endl << datasum[4] << "!=" << redata[4];
+        cout << "    Error!";
+    }
+    else{
+        int sum = 0;
+        for (int i = 0; i < 4; i++)
+            sum += redata[i] - 48;
+        for (int i = 0; i < 5; i++)
+            cout << redata[i];
+        cout << endl << datasum[4] << "!=" << sum % 10;
+        cout << "    Error!";
+    }
+}
