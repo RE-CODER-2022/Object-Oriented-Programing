11247af974d173114184a6e66efd9e0d54fca9f5
marahohc <joharam914@gmail.com>
2022-01-29 13:37:42 +0900

    끝말잇기 수정본

--- 과제제출/marahohc/04/01.cpp
+++ 과제제출/marahohc/04/01.cpp
@@ -6,17 +6,17 @@ using namespace std;
 
 class Node {
 private:
-    char * word;
+    char* word;
     Node* next;
 public:
-    Node(){
+    Node() {
         word = NULL;
         next = NULL;
     }
-    Node* getnext(){return next;}
-    void setnext(Node* NEXT){next= NEXT;}
-    void setword(char* WORD) { word = WORD;}
-    char* getword(){return word;}
+    Node* getnext() { return next; }
+    void setnext(Node* NEXT) { next = NEXT; }
+    void setword(char* WORD) { word = WORD; }
+    char* getword() { return word; }
 };
 
 class List {
@@ -25,34 +25,44 @@ private:
 public:
     List(void) { head = NULL; }
     //~List(void); // 소멸자
-    void InsertNode(char * word);
+    void InsertNode(char* word);
     void DisplayList();
     Node* gethead() { return head; }
     int Compare(char* word);
+
+    bool transword(char* firstword, char* addword) {
+        int len = strlen(firstword) - 1;
+        char fword = firstword[len] < 97 ? firstword[len] + 32 : firstword[len];
+        char aword = addword[0] < 97 ? addword[0] + 32 : addword[0];
+        if (fword == aword)
+            return true;
+        else
+            return false;
+    }
 };
 
 void List::DisplayList(void) {
     Node* cur = NULL;
     cur = head;
 
-    if(cur->getnext() == NULL)
-        cout << cur->getword() << "->"<<endl;
-    else{
-        while (cur!= NULL){
+    if (cur->getnext() == NULL)
+        cout << cur->getword() << "->" << endl;
+    else {
+        while (cur != NULL) {
             cout << cur->getword() << "-> "; //wow ->
             cur = cur->getnext();
         }
     }
     cout << endl;
 }
-void List::InsertNode(char * word) {
+void List::InsertNode(char* word) {
     //새로운 노드 생성
     Node* newNode = new Node;
     newNode->setword(word);
     //curr노드가 head에서 시작
     Node* currNode = head;
     //head가 null인 경우 head를 new노드로 지정
-    if(head == NULL){
+    if (head == NULL) {
         head = newNode;
         return;
     }
@@ -67,9 +77,9 @@ void List::InsertNode(char * word) {
 
 int List::Compare(char* curword) {
     Node* nodeinst = head;
-    char temp1[2] = {"\0"};
-    char temp2[2] = {"\0"};
-    if(nodeinst->getnext() == NULL){
+    char temp1[2] = { "\0" };
+    char temp2[2] = { "\0" };
+    if (nodeinst == NULL) {
         bool comp = 1;
 
         comp = strcasecmp(curword, nodeinst->getword());
@@ -86,8 +96,8 @@ int List::Compare(char* curword) {
         }
         else
             return 0;
-        }
-    else{ //node 두 개 이상일 때
+    }
+    else { //node 두 개 이상일 때
         while (nodeinst->getnext() != NULL) {
             int comp = 0;
             comp = strcasecmp(curword, nodeinst->getword());
@@ -100,45 +110,24 @@ int List::Compare(char* curword) {
             }
             nodeinst = nodeinst->getnext();
         }
-        if(nodeinst->getword()[strlen(nodeinst->getword()) - 1] >= 65 && nodeinst->getword()[strlen(nodeinst->getword()) - 1]<=90 || curword[0]>=65&&curword[0]<=90){
-            temp1[0] = nodeinst->getword()[strlen(nodeinst->getword()) - 1];
-            temp2[0] = curword[0];
-            
-            if (!strcasecmp(temp1,temp2))
-                return 1;
-            else if (strcasecmp(temp1,temp2)) {
-                if(strcasecmp(curword, nodeinst->getword()) == 0){
-                    cout << "Already Exists" << endl;
-                }
-                cout << "Not chained" << endl;
-                return 0;
-            }
-        else 
-            return 0;
-        }
-        if (nodeinst->getword()[strlen(nodeinst->getword()) - 1] == *(curword+0))
+        if (List::transword(nodeinst->getword(), curword))
             return 1;
-        else if (nodeinst->getword()[strlen(nodeinst->getword()) - 1] != curword[0]) {
-            if(strcasecmp(curword, nodeinst->getword()) == 0){
-                cout << "Already Exists" << endl;
-            }
+        else {
             cout << "Not chained" << endl;
             return 0;
         }
-        else 
-            return 0;
     }
 }
 
 
-int main(void){
+int main(void) {
     List* list = new List;
     char* instword = new char[20];
     bool comp = 0;
     cout << "CMD(Word/exit)>>";
     cin >> instword;
     comp = strcmp(instword, "exit");
-    if(comp == false)
+    if (comp == false)
         return 0;
     list->InsertNode(instword);
     list->DisplayList();
@@ -151,13 +140,13 @@ int main(void){
             return 0;
         else if (comp == true)
         {
-           int i = 0;
-           i = list->Compare(instword);
-           if (i == 0)
+            int i = 0;
+            i = list->Compare(instword);
+            if (i == 0)
             {
-              continue;
+                continue;
             }
-           else if (i == 1)
+            else if (i == 1)
             {
                 list->InsertNode(instword);
                 list->DisplayList();
